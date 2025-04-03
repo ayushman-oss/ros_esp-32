010d26887b4ed9e72930e6b40f3581f7a465b415
ayushman-oss <ayushman1044@gmail.com>
2025-04-03 20:10:54 +0530

    new file:   arduino-ide/eps32_bot_commands/eps32_bot_commands.ino 	new file:   arduino-ide/esp32_ros_node/esp32_ros_node.ino

---
+++ arduino-ide/eps32_bot_commands/eps32_bot_commands.ino
@@ -0,0 +1,129 @@
+#include <WiFi.h>
+#include <ESPAsyncWebServer.h>
+
+
+const char* ssid = "Ayken";
+const char* password = "Jake1044";
+
+const int leftP = 25;
+const int leftN = 26;
+const int rightP = 27;
+const int rightN = 14;
+
+
+
+AsyncWebServer server(80);
+
+// WebSocket on "/ws" endpoint
+AsyncWebSocket ws("/ws");
+
+// Function prototypes
+void handleCommand(String command);
+
+void setup() {
+  Serial.begin(115200);
+
+  WiFi.softAP(ssid, password);
+  
+
+  IPAddress IP = WiFi.softAPIP();
+  Serial.print("Soft AP IP Address: ");
+  Serial.println(IP);
+  ws.onEvent(onWsEvent);
+  server.addHandler(&ws);
+  server.begin();
+  
+  pinMode(leftP, OUTPUT);
+  pinMode(leftN, OUTPUT);
+  pinMode(rightP, OUTPUT);
+  pinMode(rightN, OUTPUT);
+  
+  stopbot();
+  
+}
+
+void loop() {
+
+}
+
+void onWsEvent(AsyncWebSocket *server, AsyncWebSocketClient *client, AwsEventType type, void *arg, uint8_t *data, size_t len) {
+  if (type == WS_EVT_CONNECT) {
+    Serial.println("Client connected");
+  } else if (type == WS_EVT_DISCONNECT) {
+    Serial.println("Client disconnected");
+  } else if (type == WS_EVT_DATA) {
+    int message = atoi((char*)data);
+    Serial.print("Received: ");
+    Serial.println(message);
+
+    // Send acknowledgment to ROS
+    client->text("Message received: " + message);
+
+
+    movement(message);
+  }
+}
+void movement(int cmdValue) {
+
+  switch (cmdValue) {
+    case 1:
+      Serial.println("Executing Forward");
+      Forward();
+      break;
+    case 3:
+      Serial.println("Executing Backward");
+      Backward();
+      break;
+    case 2:
+      Serial.println("Executing Left Turn");
+      Left();
+      break;
+    case 4:
+      Serial.println("Executing Right Turn");
+      Right();
+      break;
+    case 5:
+      Serial.println("Stopping");
+      stopbot();
+      break;
+    default:
+      Serial.println("Unknown Command ++ stopping");
+      stopbot();
+      break;
+  }
+}
+
+void Forward() {
+  digitalWrite(leftP, HIGH);
+  digitalWrite(leftN, LOW);
+  digitalWrite(rightP, HIGH);
+  digitalWrite(rightN, LOW);
+}
+
+void Backward() {
+  digitalWrite(leftP, LOW);
+  digitalWrite(leftN, HIGH);
+  digitalWrite(rightP, LOW);
+  digitalWrite(rightN, HIGH);
+}
+
+void Left() {
+  digitalWrite(leftP, LOW);
+  digitalWrite(leftN, HIGH);
+  digitalWrite(rightP, HIGH);
+  digitalWrite(rightN, LOW);
+}
+
+void Right() {
+  digitalWrite(leftP, HIGH);
+  digitalWrite(leftN, LOW);
+  digitalWrite(rightP, LOW);
+  digitalWrite(rightN, HIGH);
+}
+
+void stopbot() {
+  digitalWrite(leftP, LOW);
+  digitalWrite(leftN, LOW);
+  digitalWrite(rightP, LOW);
+  digitalWrite(rightN, LOW);
+}
